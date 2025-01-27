3d242c4a6d7c625c0321f1d1b078e8165232ce0d
Danny Ruch <danny.ruch@phamos.eu>
2025-01-27 09:34:35 +0000

    added sales order events for save and submit :: updated checkVat function to handle new requests :: this is not a final state and should not merged into any branch for now

--- edevis/public/js/sales_order.js
+++ edevis/public/js/sales_order.js
@@ -4,4 +4,52 @@ frappe.ui.form.on("Sales Order", {
             frm.set_value("terms", "");
         }
     },
+
+    before_save: function(frm) {	
+        var customerName = frm.doc.customer;
+
+        if(customerName) {
+            frappe.call({
+                method: "edevis.custom_scripts.custom_python.checkvat.checkvat",
+                args: {                    
+                  name: customerName,
+                  tax_id: '',
+                  address: '',
+                  fromDoctype: 'Sales Order'
+                },
+                freeze: true,
+                freeze_message: __('Retrieving VAT Information from server...'),
+                callback: function(r) {
+                  frappe.msgprint(r)
+                }
+              }); 
+
+        } else {  
+          frappe.msgprint(_("Customer is missing"))
+        }
+	},
+
+  before_submit: function(frm) {	
+    var customerName = frm.doc.customer;
+
+    if(customerName) {
+        frappe.call({
+            method: "edevis.custom_scripts.custom_python.checkvat.checkvat",
+            args: {                    
+              name: customerName,
+              tax_id: '',
+              address: '',
+              fromDoctype: 'Sales Order'
+            },
+            freeze: true,
+            freeze_message: __('Retrieving VAT Information from server...'),
+            callback: function(r) {
+              frappe.msgprint(r)
+            }
+          }); 
+
+    } else {  
+      frappe.msgprint(_("Customer is missing"))
+    }
+}
 });
