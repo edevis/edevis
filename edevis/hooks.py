806a2285cd97b38dc16c69afd57d597248ee7e49
frappe <frappe@copyerp.e1.edevis.com>
2025-09-22 15:36:15 +0200

    hooks.py - removed communication hook > Preventing from other emails not being created Removed email_hooks.py - useless, replaced by api/swdownload_api.py

--- edevis/hooks.py
+++ edevis/hooks.py
@@ -165,12 +165,6 @@ doc_events = {
     },
     "Supplier": {
         "after_insert": "edevis.custom_scripts.custom_python.supplier.after_insert"
-    },
-    # "Software Download Request": {
-	# 	"after_insert": "edevis.custom_scripts.custom_python.downloads.send_download_links"
-	# },
-    "Communication": {
-        "after_insert": "edevis.custom_scripts.custom_python.email_hooks.handle_software_download_email"
     }
 }
 
